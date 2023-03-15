a113542ede2125c8c278695cb241f25e9b17f57a
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-03-15 13:15:40 -0700

    entered valid coordinate condition in placeShip function

--- src/index.js
+++ src/index.js
@@ -14,34 +14,39 @@ const Gameboard = () => {
 
   const generateBoard = () => {
     for (let i = 0; i < 10; i++) {
-      board[i] = [i];
+      board[i] = [];
       for (let j = 0; j < 10; j++) {
-        board[i][j] = j;
+        board[i][j] = 0;
       }
     }
     return board;
   };
 
-  // creates a ship of a specific length  when called
-  const generateShip = (length) => {
-    return ship(length);
+  const gameBoard = generateBoard();
+
+  const placeShip = (shipLength, coordinate) => {
+    // suppose you want to place a destroyer in [5, 5] how would you do this?
+    let x = coordinate[0];
+    let y = coordinate[1];
+    // places the hsip horizontally from starting coordinate towards the right most end of the ship
+
+    // write a function that checks the availability of the coordinates
+    // start by checking if the present coordinate is available;
+
+    for (let i = 0; i < shipLength; i++) {
+      if (gameBoard[x][y] === 1) {
+        return "this coordinate is taken";
+      }
+      gameBoard[x][y + i] = 1;
+    }
+    return gameBoard;
   };
 
-  const placeShip = (shipType, coordinates) => {
-    const shipLength = shipType.length;
-
-    const placeFunction = (shipLength, coordinates) => {
-      //code that iterates through gameBoard and returns a `shipLength` number of consecutive coordinates
-      // starting from `coordinates`
-      return coord;
-    };
-  };
-  return { generateBoard, generateShip, placeShip };
+  return { generateBoard, placeShip, gameBoard };
 };
 
 const game = Gameboard();
 
-console.log(game.placeShip(cruiser));
-
+console.log(game.placeShip(2, [1, 1]));
 // Gameboards should be able to place ships at specific coordinates
 // by calling the ship factory function.
